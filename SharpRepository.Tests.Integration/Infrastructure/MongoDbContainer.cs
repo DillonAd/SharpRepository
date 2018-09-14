a81d34a1d9efb7d519935a93b0f81e9e6e24c5a9
Dillon Adams <dillon-a@hotmail.com>
2018-09-13 19:32:42 -0500

    Adding initial version of the MongoDB implementation of ContainerService

---
+++ SharpRepository.Tests.Integration/Infrastructure/MongoDbContainer.cs
@@ -0,0 +1,8 @@
+﻿using NUnit.Framework;
+namespace SharpRepository.Tests.Integration.Infrastructure
+{
+    public class MongoDbContainer : ContainerService
+    {
+        public MongoDbContainer() : base(@"npipe://./pipe/docker_engine", "mongo", "latest", "sr-mongodb", 27017) { }
+    }
+}
