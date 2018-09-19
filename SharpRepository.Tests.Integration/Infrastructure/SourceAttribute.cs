bea82747a46f7f88d70cbe7ebe708f8f7537302d
Dillon Adams <dillon-a@hotmail.com>
2018-09-18 22:10:18 -0500

    Adding TestCaseSourceAttribute base class as an intermediary class so containers can start before the TestCase data is assembled

---
+++ SharpRepository.Tests.Integration/Infrastructure/SourceAttribute.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SharpRepository.Tests.Integration.Infrastructure
+{
+    public class SourceAttribute : TestCaseSourceAttribute
+    {
+        static SourceAttribute()
+        {
+            ContainerService.Start().Wait();
+        }
+
+        public SourceAttribute(Type type, string source) : base(type, source) { }
+    }
+}
